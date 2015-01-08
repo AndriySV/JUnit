51dcbaa1c6b019cb0cbce387343391e16c30f08f
AndriySV <A.S.Elips@gmail.com>
2015-01-08 18:13:20 +0200

    add the environmentSetup

---
+++ src/main/java/com/as/junit/environmentSetup/TestJunit.java
@@ -0,0 +1,13 @@
+package com.as.junit.environmentSetup;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJunit {
+	@Test
+	public void testAdd() {
+		String str = "Junit is working fine";
+		assertEquals("Junit is working fine", str);
+	}
+}
