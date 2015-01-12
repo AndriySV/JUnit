ae1b9a3bc0a75cd29b93813907a839b2ab6a8dfa
AndriySV <svyryd.andriy@gmail.com>
2015-01-12 10:39:55 +0200

    Added the suiteTest

---
+++ src/main/java/com/as/testNG/suiteTest/Test1.java
@@ -0,0 +1,15 @@
+package com.as.testNG.suiteTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	String message = "Manisha";
+	MessageUtil messageUtil = new MessageUtil(message);
+
+	@Test
+	public void testPrintMessage() {
+		System.out.println("Inside testPrintMessage()");
+		Assert.assertEquals(message, messageUtil.printMessage());
+	}
+}
