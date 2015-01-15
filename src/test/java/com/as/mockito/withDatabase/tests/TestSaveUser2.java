cc104bb9153ad9c4d5a1d749414be47ebe31b54a
AndriySV <svyryd.andriy@gmail.com>
2015-01-15 14:05:24 +0200

    Added the TestSaveUser2

--- src/test/java/com/as/mockito/withDatabase/tests/TestSaveUser2.java
+++ src/test/java/com/as/mockito/withDatabase/tests/TestSaveUser2.java
@@ -4,37 +4,35 @@ import static org.mockito.Mockito.*;
 
 import org.testng.annotations.Test;
 
-
-
 import com.as.mockito.withDatabase.database.User;
 import com.as.mockito.withDatabase.database.UserDao;
 
 import static org.testng.Assert.*;
 
-public class TestSaveUser {
+public class TestSaveUser2 {
 	private UserDao userDao = new UserDao();
-	
+
 	@Test
 	public void testSaveUser() {
 		User user = new User();
 		user.setId(3);
 		user.setName("User3");
 		user.setAge(3);
-		
+
 		userDao.saveUser(user);
-		
+
 		assertEquals(user, userDao.findById(3));
-		
+
 		userDao.deleteUser(user);
 	}
-	
+
 	@Test
 	public void testMockitoSaveUser() {
 		User user = mock(User.class);
-		
+
 		when(user.getName()).thenReturn("User1");
-		
+
 		assertEquals(user.getName(), userDao.findById(1).getName());
 	}
-	
+
 }
